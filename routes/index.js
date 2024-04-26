f5a6bcd22a98e4139f6a673cee0ea99670756063
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2024-04-26 16:30:42 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,29 @@
 // Importing Express framework and setting up a router
-const express= require('express')
-const router=express.Router()
-
-
-// Middleware for parsing request bodies (form data)
-router.get('/', function(req,res,next){
-     // Renders the 'index' view template with provided options
-    res.render('index',{title:'Feedback app',author:'nam'})
+const express = require('express')
+const router = express.Router()
+// Route to handle GET requests for the homepage
+router.get('/', function(req, res, next) {
+    res.render('index', {title: 'Feedback app', author: 'nam'})
 })
- // Route to handle GET requests for the feedback form page
-router.get('/feedback-form', function(req,res,next){
-    // Renders the 'student_feedback_form' view template without any specific options
-    res.render('studen_feedback_form')
+// Route to handle GET requests for the feedback form page
+router.get('/feedback-form', function(req, res, next){
+    res.render('student_feedback_form')
 })
 // Route to handle POST requests when a feedback form is submitted
-router.post('/submit-feedback',function(req,res,next){
-  
-    const formData= req.body // Extracts data from the form submission
-
+router.post('/submit-feedback', function(req, res, next){
    
-  // Renders the 'thank_you' view template with options passed from the form data  
-    res.render('thank_you',{
-          name: formData.name,// Form input for name
-          email: formData.email,// Form input for email
-          message: formData.comments, // Form input for comments
-          currentSudent: formData['current-student']// Form input to check if the user is a current student
+    const formData = req.body// Extracts data from the form submission
+    
+    
+// Renders the 'thank_you' view template with options passed from the form data
+    res.render('thank_you', { 
+        name: formData.name, // Form input for name
+        email: formData.email, // Form input for email
+        message: formData.comments,// Form input for comments
+        currentStudent: formData['current-student'] 
+     })// Form input to check if the user is a current student
 
-    })
 })
 // Export the router so it can be included in the main app
-module.exports=router
+module.exports = router
+
--- server.js
+++ server.js
@@ -1,31 +1,29 @@
-const express =require('express')
-const path=require('path')
-const bodyParser=require('body-parser')
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
 
-//import the routes in routes/index.js
-const indexRouter=require('./routes/index')
+// import the routes in routes/index.js
+const indexRouter = require('./routes/index')
 
-//create the web application
-const app=express()
+// Create the web application
+const app = express() 
 
-//enable parsing of POST request form body
-app.use(bodyParser.urlencoded({ extended: false}))
+// enable parsing of POST request form body
+app.use(bodyParser.urlencoded({ extended: false }))
 
-//serve static files,such as CSS
-const staticFilelocation =path.join(__dirname,'public')
-app.use(express.static(staticFilelocation))
+// serve static files, such as CSS
+const staticFileLocation = path.join(__dirname, 'public')
+app.use(express.static(staticFileLocation))
 
+// configure it to use the Handlebars template engine and 
+// work with template files in the views directory 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
-
-//configure  it to use  the handlebars template engine  and 
-// work  with template files  in views directory 
-app.set ('views',path.join(__dirname,'views'))
-app.set('view engine','hbs');
-
-// make the routes in index.js available to the app
-app.use('/',indexRouter)
+// make the routes in index.js available to the app 
+app.use('/', indexRouter)
 
 // Start server running 
-let server=app.listen (process.env.PORT ||  3000 ,function () {
- console.log('express server ruinning on port',server.address().port)
+let server = app.listen(process.env.PORT || 3000, function() {
+    console.log('Express server running on port', server.address().port)
 })
