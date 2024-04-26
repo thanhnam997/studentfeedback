375865394292961e3234508f1eac24fc7676c337
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2024-04-26 15:33:30 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,32 @@
+// Importing Express framework and setting up a router
 const express= require('express')
 const router=express.Router()
 
+
+// Middleware for parsing request bodies (form data)
 router.get('/', function(req,res,next){
-     res.render('index',{title:'Feedback app',author:'nam'})
+     // Renders the 'index' view template with provided options
+    res.render('index',{title:'Feedback app',author:'nam'})
 })
- 
+ // Route to handle GET requests for the feedback form page
 router.get('/feedback-form', function(req,res,next){
+    // Renders the 'student_feedback_form' view template without any specific options
     res.render('studen_feedback_form')
 })
-
+// Route to handle POST requests when a feedback form is submitted
 router.post('/submit-feedback',function(req,res,next){
-    //get the form data from the URL query
-    const formData= req.body
+  
+    const formData= req.body // Extracts data from the form submission
 
+   
+  // Renders the 'thank_you' view template with options passed from the form data  
     res.render('thank_you',{
-          name: formData.name,
-          email: formData.email,
-          message: formData.comments,
-          currentSudent: formData['current-student']
+          name: formData.name,// Form input for name
+          email: formData.email,// Form input for email
+          message: formData.comments, // Form input for comments
+          currentSudent: formData['current-student']// Form input to check if the user is a current student
 
     })
 })
-
+// Export the router so it can be included in the main app
 module.exports=router
